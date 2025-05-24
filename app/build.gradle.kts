f0c684059299cbdf42882bab9b427ce96c978c5c
Arnufo Rodríguez <luis.rodriguez@lubelsoft.com>
2025-05-24 10:01:49 -0600

    Sesion de Actualizacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,7 +43,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.1.4")
+    implementation("com.google.firebase:firebase-core:21.1.1")
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
--- app/src/main/java/com/lubelsoft/miaplicacion101/app/FirstActivity.kt
+++ app/src/main/java/com/lubelsoft/miaplicacion101/app/FirstActivity.kt
@@ -5,31 +5,71 @@ import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
 import com.lubelsoft.miaplicacion101.R
+import okhttp3.OkHttpClient
+import org.json.JSONObject
 
 class FirstActivity : AppCompatActivity() {
 
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_first)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        val btnLogin= findViewById<AppCompatButton>( R.id.btnLogin)
+        val usernameEditText = findViewById<AppCompatEditText>(R.id.username)
+
+        btnLogin.setOnClickListener(){
+            val username = usernameEditText.text.toString()
+            Log.i("miLogs","se ah pulsado el boton")
+            Log.i("miLogs", "Texto ingresado: $username")
+            loginUser(username)
+            //testConexion()
+
         }
 
-        val btnOne = findViewById<AppCompatButton>(R.id.btnOne)
 
-        btnOne.setOnClickListener() {
-            Log.i("Ciclo11", "Se ha pulsado el botón")
-        }
+    }
+    private fun testConexion(){
+        db = FirebaseFirestore.getInstance()
+        db.collection("test").add(mapOf("timestamp" to System.currentTimeMillis()))
+    }
+
+    private fun loginUser(username:String){
+
+        //firebase-adminsdk-fbsvc@cliclo11-firebase.iam.gserviceaccount.com
+
+        val cliente = OkHttpClient()
+        val jsonObj = JSONObject()
 
 
+        db = FirebaseFirestore.getInstance()
 
+        db.collection("users")
+            .whereEqualTo("username", username)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    Log.d("miLogs", "No se encontraron usuarios con el username: $username")
+                    // Maneja el caso donde no se encuentra ningún usuario
+                } else {
+                    for (document in documents) {
+                        Log.d("miLogs", "${document.id} => ${document.data}")
 
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("miLogs", "Error al obtener documentos: ", exception)
+                // Maneja el error
+            }
 
     }
 }
